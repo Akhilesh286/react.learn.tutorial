c3f14c59986df9f241df14aafb2402188d56a134
Akhilesh286 <h3356335@gmail.com>
2025-06-24 20:30:29 +0530

    coverd first topic called custom hook and build a custom hook called windowSize that return size of the currrent window

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import useWindowSize from './tutorials/customHook/useWindowWidth'
 
 function App() {
+  const [width,height] = useWindowSize()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{width}</p>
+      <p>{height}</p>
     </div>
   );
 }
